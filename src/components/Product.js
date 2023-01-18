c5aafbf729254499f384799fba8e2288a5ee03d5
taranovska <93533926+taranovska@users.noreply.github.com>
2023-01-18 16:10:06 +0200

    add product card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./Product.css";
 
-const Product = () => {
+function Product({ id, title, image, price, rating }) {
   return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>Title</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>19.99</strong>
-          </p>
-          <div className="product__rating">
-            <p>⭐</p>
-          </div>
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          alt=""
-        />
-        <button>Add to Basket</button>
       </div>
+      <img src={image} alt="" />
+      <button>Add to Basket</button>
     </div>
   );
-};
+}
 
 export default Product;
